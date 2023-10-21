dab461c43efda4fbecaaa3a4e2235fd44a9ebe8c
Abraham12k12 <cheksum04.s@gmail.com>
2023-10-20 22:45:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/esasycomplex/TrigonoConversion.kt
@@ -0,0 +1,11 @@
+package com.example.esasycomplex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrigonoConversion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trigono_conversion)
+    }
+}
